35cacd14e7b01ba344bf374932e5165b4b3c101a
mantoniio <m.antoniio1995@gmail.com>
2023-11-29 09:57:49 -0300

    adicionando nova rota

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('<h1>To Do List</h1>');
 });
 
+app.get('/json', (req, res) => {
+  res.json({ message: 'Tarefa x', done: true });
+});
+
 app.listen(3000, () => {
   console.log('Servidor iniciado http://localhost:3000');
 });
